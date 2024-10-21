71d2fe55d8af0501e26a9112dcca2c6344e7d92d
pratikkshirsagar <meetpratiik@gmail.com>
2024-10-21 18:42:23 +0530

    fizz buzz solution added

---
+++ arrays/basic/fizzbuzz.js
@@ -0,0 +1,24 @@
+// In this challenge, you will write a function called `fizzBuzzArray` that takes in a number and returns an array. The array should contain all the numbers from 1 to the number passed in. However, if the number is divisible by 3, you should replace the number with "Fizz". If the number is divisible by 5, you should replace the number with "Buzz". If the number is divisible by both 3 and 5, you should replace the number with "FizzBuzz".
+
+function fizzBuzzArray(num) {
+  const arr = [];
+
+  for (let i = 1; i <= num; i++) {
+    console.log(i);
+
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      arr.push('Fizz');
+    } else if (i % 5 === 0) {
+      arr.push('Buzz');
+    } else {
+      arr.push(i);
+    }
+  }
+
+  return arr;
+}
+
+const result = fizzBuzzArray(15);
+console.log(result);
